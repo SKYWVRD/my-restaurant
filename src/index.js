fb8223e953867ac137bd57753acf7e553cd5c63e
Sean Boonzaier <boonzaiersa@gmail.com>
2022-09-01 21:33:27 +0200

    Add simple nav builder

--- src/index.js
+++ src/index.js
@@ -1,6 +1,40 @@
 import {
     buildPage
-} from "./main-page.js"
+} from "./home-page.js";
+
+import './styles.css';
 
 
-buildPage('Jack');
+buildNav();
+
+
+
+
+
+
+
+
+
+function buildNav(){
+    const contentContainer = document.getElementById('content');
+    const navContainer = document.createElement('div');
+    navContainer.classList.add('nav-container');
+    const homeTab = document.createElement('div');
+    const menuTab = document.createElement('div');
+    const contactTab = document.createElement('div');
+
+    homeTab.classList.add('tab');
+    menuTab.classList.add('tab');
+    contactTab.classList.add('tab');
+
+    homeTab.innerHTML = 'HOME';
+    menuTab.innerHTML = 'MENU';
+    contactTab.innerHTML = 'CONTACT';
+
+    navContainer.appendChild(homeTab);
+    navContainer.appendChild(menuTab);
+    navContainer.appendChild(contactTab);
+    
+
+    contentContainer.appendChild(navContainer);
+}
--- src/main-page.js
+++
@@ -1,6 +0,0 @@
-export const buildPage = (name) => {
-    const mainContent = document.getElementById('content');
-    
-    mainContent.innerHTML = `Hello ${name}`;
-}
-
