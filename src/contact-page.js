becf0919f1a397c5744bbb3d0c7ba9f91f730626
Sean Boonzaier <boonzaiersa@gmail.com>
2022-09-04 15:38:01 +0200

    Finalize linking

--- src/contact-page.js
+++ src/contact-page.js
@@ -6,8 +6,12 @@ export const buildContacts = () => {
     const mainContainer = document.getElementById('content');
     clearContent();
     const contactList = document.createElement('div')
+    const contactTitle = document.createElement('div')
+    const writeUp = document.createElement('div');
     contactList.classList.add('blurb')
-    contactList.innerHTML = 'This is a contact page';
+    contactTitle.classList.add('title')
+    contactTitle.innerHTML = 'Contact'
 
+    contactList.appendChild(contactTitle);
     mainContainer.appendChild(contactList);
 }
