14848c911f1c596bc7698c6910203898f8823c0d
Sean Boonzaier <boonzaiersa@gmail.com>
2022-09-04 12:28:44 +0200

    Fix content child removal code

--- src/util.js
+++ src/util.js
@@ -1,5 +1,5 @@
 export function clearContent() {
     const mainContainer = document.getElementById('content');
-    if(mainContainer.childNodes.length > 1)
-        mainContainer.removeChild(mainContainer.childNodes[1]);
+    if(mainContainer.childNodes.length >= 1)
+        mainContainer.removeChild(mainContainer.childNodes[0]);
 }
